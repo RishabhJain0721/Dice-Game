8828ac80cd8a13df39fc328e36ab209e1d599767
Rishabh Jain <107767172+RishabhJain0721@users.noreply.github.com>
2022-09-04 00:03:51 +0530

    Image paths updated.

--- die.js
+++ die.js
@@ -5,8 +5,8 @@ no2=Math.ceil(no2*6);
 console.log(no1);
 console.log(no2);
 function myLoad(){
-    document.querySelector(`.img1`).setAttribute(`src`,`images/dice${no1}.png`);
-    document.querySelector(`.img2`).setAttribute(`src`,`images/dice${no2}.png`);
+    document.querySelector(`.img1`).setAttribute(`src`,`utils/dice${no1}.png`);
+    document.querySelector(`.img2`).setAttribute(`src`,`utils/dice${no2}.png`);
 }
 window.onload=myLoad;
 if(no1>no2){
