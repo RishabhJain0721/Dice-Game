b9b5239edfeb67c7f3c2befc5ef9c5f698c9cd23
Rishabh Jain <107767172+RishabhJain0721@users.noreply.github.com>
2022-09-03 23:47:29 +0530

    Add files via upload

---
+++ die.js
@@ -0,0 +1,20 @@
+let no1 = Math.random();
+no1=Math.ceil(no1*6);
+let no2 = Math.random();
+no2=Math.ceil(no2*6);
+console.log(no1);
+console.log(no2);
+function myLoad(){
+    document.querySelector(`.img1`).setAttribute(`src`,`images/dice${no1}.png`);
+    document.querySelector(`.img2`).setAttribute(`src`,`images/dice${no2}.png`);
+}
+window.onload=myLoad;
+if(no1>no2){
+    document.querySelector(`.title`).innerHTML=`Player 1 Wins!`;
+}
+else if(no1<no2){
+    document.querySelector(`.title`).innerHTML=`Player 2 Wins!`;
+}
+else if(no1===no2){
+    document.querySelector(`.title`).innerHTML=`Draw!`;
+}
